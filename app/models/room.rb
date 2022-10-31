18734433a2c6bdf4f10401242112bbcc6564e83d
Rodrigo - Xita rps <xitarps@gmail.com>
2022-10-31 01:25:26 -0300

    Add subcribe with broadcast room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  # the pages listening to rooms_list will receive updates
+  broadcasts_to ->(room){:rooms_list}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "rooms#index"
+  devise_for :users
   resources :rooms
 end
